b90d72ead6d79869f6dc19f716fe8ed501555615
Chris Mazanec <chris@pristin.io>
2024-10-12 17:13:20 +0200

    external-url-support #comment added external url browser support

--- main.ts
+++ main.ts
@@ -9,73 +9,68 @@ const DEFAULT_SETTINGS: Settings = {
 }
 
 export default class SelectOpenPlugIn extends Plugin {
-	settings: Settings;
+    settings: Settings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		this.addSettingTab(new SettingTab(this.app, this));
+        this.addSettingTab(new SettingTab(this.app, this));
 
-		this.addCommand({
-			id: 'select-open-links',
-			name: 'Select Open Links',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				this.selectOpenTexts(view)
-			}
-		});
-	}
+        this.addCommand({
+            id: 'select-open-links',
+            name: 'Select Open Links',
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                this.selectOpenTexts(view)
+            }
+        });
+    }
 
-	selectOpenTexts(view: MarkdownView) {
-		var files = this.getFilesName(view);
+    selectOpenTexts(view: MarkdownView) {
+        var items = this.getItemsToOpen(view);
 
-		files.forEach((fileName) => {
-			if(fileName) this.app.workspace.openLinkText(fileName, "", this.settings.paneType, {active: false})
-		})
-	}
-		
-	// https://github.com/mrjackphil/obsidian-crosslink-between-notes
-	getFilesName(view: MarkdownView) {
-		const cm = view.editor
-        const cursor = cm.getCursor()
-        const selectedRange = cm.getSelection()
-        const line = selectedRange || cm.getLine(cursor.line)
-
-        const regexpWiki = /\[\[.+?]]/gi
-        const linksWiki = line.match(regexpWiki) || []
-
-        const ar = [linksWiki].filter(e => e.length)
-
-        return ar.flat().map((lnk) => {
-            const wikiName = lnk
-                .replace(/(\[\[|]])/g, '')
-                .replace(/\|.+/, '')
-                .replace(/#.+/, '')
-
-            return wikiName
+        items.forEach((item) => {
+            if (item.type === 'file' && item.name) {
+                this.app.workspace.openLinkText(item.name, "", this.settings.paneType, {active: false})
+            } else if (item.type === 'url' && item.url) {
+                window.open(item.url, '_blank');
+            }
         })
-	}
-
-	getFilesFromLineOrSelection(view: MarkdownView): TFile[] {
+    }
+        
+    getItemsToOpen(view: MarkdownView) {
         const cm = view.editor
         const cursor = cm.getCursor()
         const selectedRange = cm.getSelection()
         const line = selectedRange || cm.getLine(cursor.line)
 
         const regexpWiki = /\[\[.+?]]/gi
-        const linksWiki = line.match(regexpWiki) || []
-        const ar = [linksWiki].filter(e => e.length)
+        const regexpUrl = /https?:\/\/[^\s\]]+(?=\))?/gi
 
-        return ar.flat().map((lnk) => {
-            const wikiName = lnk
-                .replace(/(\[\[|]])/g, '')
-                .replace(/\|.+/, '')
-                .replace(/#.+/, '')
-			
-            return this.getFilesByName(wikiName)
-        })
+        const linksWiki = line.match(regexpWiki) || []
+        const linksUrl = line.match(regexpUrl) || []
+
+        const wikiItems = linksWiki.map(link => ({
+            type: 'file',
+            name: link.replace(/(\[\[|]])/g, '').replace(/\|.+/, '').replace(/#.+/, '')
+        }));
+
+        const urlItems = linksUrl.map(url => ({
+            type: 'url',
+            url: url.endsWith(')') ? url.slice(0, -1) : url
+        }));
+
+        return [...wikiItems, ...urlItems];
     }
 
-	getFilesByName(name: string | string[]) {
+    getFilesFromLineOrSelection(view: MarkdownView): TFile[] {
+        const items = this.getItemsToOpen(view);
+        return items
+            .filter(item => item.type === 'file')
+            .map(item => this.getFilesByName(item.name as string))
+            .filter(file => file !== undefined) as TFile[];
+    }
+
+    getFilesByName(name: string | string[]) {
         const files = this.app.vault.getFiles()
 
         if (Array.isArray(name)) {
@@ -91,17 +86,17 @@ export default class SelectOpenPlugIn extends Plugin {
         )[0]
     }
 
-	onunload() {
+    onunload() {
 
-	}
+    }
 
-	async loadSettings() {
+    async loadSettings() {
         this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
     }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
 class SettingTab extends PluginSettingTab {
@@ -117,17 +112,17 @@ class SettingTab extends PluginSettingTab {
 
         containerEl.empty();
         containerEl.createEl('h2', {text: 'Settings for Open Selected Links plugin'});
-		
-		new Setting(containerEl)
-			.setName('Select a procedure of opening a linked document')
-			.addDropdown(dropDown => {
-				dropDown.addOption('tab', 'Tab');
-				dropDown.addOption('window', 'Window');
-				dropDown.addOption('split', 'Split');
-				dropDown.onChange(async (value) =>	{
-					this.plugin.settings.paneType = (value as PaneType);
-					await this.plugin.saveSettings();
-				});
-			});
+        
+        new Setting(containerEl)
+            .setName('Select a procedure of opening a linked document')
+            .addDropdown(dropDown => {
+                dropDown.addOption('tab', 'Tab');
+                dropDown.addOption('window', 'Window');
+                dropDown.addOption('split', 'Split');
+                dropDown.onChange(async (value) =>    {
+                    this.plugin.settings.paneType = (value as PaneType);
+                    await this.plugin.saveSettings();
+                });
+            });
     }
 }
